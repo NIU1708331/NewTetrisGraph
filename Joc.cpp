914882ddd1ac695d568c0fd65831d544c7cfc9c0
NIU1671000 <116224779+NIU1671000@users.noreply.github.com>
2024-04-28 20:42:51 +0200

    Update Joc.cpp

--- Joc.cpp
+++ Joc.cpp
@@ -5,8 +5,30 @@ using namespace std;
 
 void Joc::inicialitza(const string& nomFitxer)
 {
-	
+	ifstream fitxerInit;
+	fitxerInit.open(nomFitxer);
+	if (fitxerInit.is_open())
+	{
+
+        fitxerInit >> tipusFigura >> fila >> columna >> tipusGir;
+
+        m_figura.setTipus(static_cast<TipusFigura>(tipusFigura));
+        m_figura.setPosicio(columna, fila);
+        m_figura.setDir(tipusGir);
+
+        int taula[MAX_FILA][MAX_COL];
+        for (int i = 0; i < MAX_FILA; ++i)
+        {
+            for (int j = 0; j < MAX_COL; ++j)
+            {
+                fitxerInit >> taula[i][j];
+            }
+        }
+        m_tauler.setTaula(taula);
+        fitxerInit.close();
+	}
 }
+
 bool Joc::giraFigura(DireccioGir direccio) 
 {
 	return 1;
